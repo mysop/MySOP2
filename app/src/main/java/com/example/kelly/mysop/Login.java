8aac61693691fb8a5a3acea11c81cc964b9170e4
KellyHsu <kelly830619@gmail.com>
2015-07-08 11:14:58 +0800

    j
    h

---
+++ app/src/main/java/com/example/kelly/mysop/Login.java
@@ -0,0 +1,53 @@
+package com.example.kelly.mysop;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void forgetpassword (View v){
+        Intent it = new Intent(this,Forget.class);
+        startActivity(it);
+    }
+
+    public void errortest (View v){
+        Intent it = new Intent(this,Error.class);
+        startActivity(it);
+    }
+
+
+}

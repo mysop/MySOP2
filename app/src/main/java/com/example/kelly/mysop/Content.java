4803ff8ec9782f186b4f9afc4695250a5dbf6212
tina8857 <tina8857@yahoo.com.tw>
2015-07-29 11:52:44 +0800

    0729
    listview動態新增 測試中

--- app/src/main/java/com/example/kelly/mysop/Content.java
+++ app/src/main/java/com/example/kelly/mysop/Content.java
@@ -1,17 +1,33 @@
 package com.example.kelly.mysop;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
 
 
 public class Content extends Activity {
 
+    private EditText inputText;
+    private ListView listInput;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> items;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+
+        inputText = (EditText)findViewById(R.id.inputText);
+        listInput = (ListView)findViewById(R.id.listInputText);
+        items = new ArrayList<String>();
+        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,items);
+        listInput.setAdapter(adapter);
     }
 
 
@@ -27,7 +43,7 @@ public class Content extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+/*        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
@@ -35,5 +51,33 @@ public class Content extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+*/
+
+        switch(item.getItemId()){
+            case Menu.FIRST:
+                if(!inputText.getText().toString().equals("")){
+                    items.add(inputText.getText().toString());
+                    listInput.setAdapter(adapter);
+                    inputText.setText("");
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void onActivityResult(int requestCode,
+                                    int resultCode, Intent it) {
+        if(resultCode == RESULT_OK) {
+            items.set(requestCode, it.getStringExtra("備忘")); // �ϥζǦ^����Ƨ�s�}�C���e
+            adapter.notifyDataSetChanged(); // �q�� Adapter �}�C���e����s
+        }
+    }
+
+    public void writeCommon (View v){
+        if(!inputText.getText().toString().equals("")){
+            items.add(inputText.getText().toString());
+            listInput.setAdapter(adapter);
+            inputText.setText("");
+        }
     }
 }

3cfc9d1fe592a46135215bed950a02de25603680
Chris <cliff5345179@gmail.com>
2015-08-04 15:38:52 +0800

    不紅的不能用
    不紅的不能用

--- app/src/main/java/com/example/kelly/mysop/Mysop.java
+++ app/src/main/java/com/example/kelly/mysop/Mysop.java
@@ -48,18 +48,18 @@ public class Mysop extends Activity {
             map.put(TAG_PID, parser.getValue(e, TAG_PID));
             map.put(TAG_PRODUCTS, parser.getValue(e, TAG_PRODUCTS));
             map.put(TAG_NAME, parser.getValue(e, TAG_NAME));
-           // map.put(KEY_DURATION, parser.getValue(e, KEY_DURATION));
-           // map.put(KEY_THUMB_URL, parser.getValue(e, KEY_THUMB_URL));
+            // map.put(KEY_DURATION, parser.getValue(e, KEY_DURATION));
+            // map.put(KEY_THUMB_URL, parser.getValue(e, KEY_THUMB_URL));
 
             // HashList添加到數組列表
             songsList.add(map);
         }
 
 
-        list=(ListView)findViewById(R.id.list);
+        list = (ListView) findViewById(R.id.list);
 
 
-        adapter=new LazyAdapter(this, songsList);
+        adapter = new LazyAdapter(this, songsList);
         list.setAdapter(adapter);
 
 
@@ -73,94 +73,10 @@ public class Mysop extends Activity {
 
                 //這裏可以自由發揮，比如播放一首歌曲等等
             }
-<<<<<<< HEAD
+
         });
-=======
 
-            return null;
-        }
-        
-        /**
-         * After completing background task Dismiss the progress dialog
-         * **/
-        protected void onPostExecute(String file_url) {
-            // dismiss the dialog after getting all products
-            pDialog.dismiss();
-            // updating UI from Background Thread
-/*            runOnUiThread(new Runnable() {
-                public void run() {
-
-                }
-            }); */
-
-
-
-            RelativeLayout w = (RelativeLayout)findViewById(R.id.relative);
-            w.setBackgroundColor(Color.parseColor("#EEFFBB"));
-            // 取得 LinearLayout 物件
-            LinearLayout l = new LinearLayout(Mysop.this);
-            l.setBackgroundResource(R.drawable.linearlayout);
-            //l.setId(0);
-
-
-            LinearLayout l2 = new LinearLayout(Mysop.this);
-            l2.setBackgroundColor(Color.parseColor("#FF8888"));
-            l2.setOrientation(LinearLayout.VERTICAL);
-            //l2.setOnClickListener();
-            TextView tv = new TextView(Mysop.this);
-            tv.setText(productsList.get(0).get(TAG_PID));
-            //tv.setText(productsList.get(0).get(TAG_PID));
-            l2.addView(tv);
-            TextView tv1 = new TextView(Mysop.this);
-            tv1.setText(productsList.get(0).get(TAG_NAME));
-            l2.addView( tv1 );
-
-            l.addView( l2 );
-
-            LinearLayout l3 = new LinearLayout(Mysop.this);
-            l3.setBackgroundColor(Color.parseColor("#99BBFF"));
-            l3.setOrientation(LinearLayout.VERTICAL);
-
-            TextView tv3 = new TextView(Mysop.this);
-            tv3.setText(productsList.get(0).get(TAG_PID));
-            l3.addView( tv3 );
-            l.addView( l3 );
-
-            //從這裡哈哈哈哈哈哈哈
-            w.addView(l);
-/*
-            LinearLayout l4 = new LinearLayout(Mysop.this);
-            l4.setBackgroundResource(R.drawable.linearlayout);
-            //l4.setLayoutParams(new RelativeLayout.LayoutParams(150, 150));
-            RelativeLayout.LayoutParams r = new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-            //r.addRule(RelativeLayout.BELOW, R.id.  );
-            r.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-
-
-            LinearLayout nl2 = new LinearLayout(Mysop.this);
-            nl2.setBackgroundColor(Color.parseColor("#FF8888"));
-            nl2.setOrientation(LinearLayout.VERTICAL);
-            //l2.setOnClickListener();
-            TextView ntv = new TextView(Mysop.this);
-            ntv.setText(productsList.get(1).get(TAG_PID));
-            nl2.addView(ntv);
-            l4.addView(nl2);
-
-            w.addView(l4,r);
-
-*/
-
-
-            AlertDialog.Builder dialog = new AlertDialog.Builder(Mysop.this);
-            dialog.setTitle("");
-            dialog.setMessage(productsList.get(0).get(TAG_PID));
-            dialog.show();
-
-
-        }
-
->>>>>>> origin/master
     }
 }
-
+        
 

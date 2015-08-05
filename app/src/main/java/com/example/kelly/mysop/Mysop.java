a23dd9008bf55e7415480f0e83433b51680b79ae
Chris <cliff5345179@gmail.com>
2015-08-05 18:32:58 +0800

    嗚嗚嗚
    嗚嗚嗚嗚嗚

--- app/src/main/java/com/example/kelly/mysop/Mysop.java
+++ app/src/main/java/com/example/kelly/mysop/Mysop.java
@@ -1,81 +1,173 @@
 package com.example.kelly.mysop;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Mysop extends Activity {
 
+
+
+    private ProgressDialog pDialog;
+    JSONParser jParser = new JSONParser();
     ArrayList<HashMap<String, String>> productsList;
     //private static String url_all_products = "http://localhost:8080/kelly/test_getall.jsp";
-    static String url_all_products = "http://140.115.80.237/front/test_getall.jsp";
-    static final String TAG_SUCCESS = "success";
-    static final String TAG_PRODUCTS = "products";
-    static final String TAG_PID = "pid";
-    static final String TAG_NAME = "name";
-    ListView list;
-    LazyAdapter adapter;
+    private static String url_all_products = "http://140.115.80.237/front/test_getall.jsp";
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_PRODUCTS = "products";
+    private static final String TAG_PID = "pid";
+    private static final String TAG_NAME = "name";
+
+    private ListView listInput;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> items;
+
+    private ImageView picture;
+    private TextView title;
+    private TextView master;
+
+    JSONArray products = null;
+
+
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mysop);
+        listInput = (ListView)findViewById(R.id.list_sop);
+        items = new ArrayList<String>();
+        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,items);
+        //listInput.setAdapter(adapter);
+
+        // Hashmap for ListView
+        productsList = new ArrayList<HashMap<String, String>>();
+        // Loading products in Background Thread
+        new LoadAllProducts().execute();
 
 
-        ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+    }
 
-        XMLParser parser = new XMLParser();
-        String xml = parser.getXmlFromUrl(url_all_products); // 從網络獲取xml
-        Document doc = parser.getDomElement(xml); // 獲取 DOM 節點
 
-        NodeList nl = doc.getElementsByTagName(TAG_PRODUCTS);
-        // 循環遍曆所有的歌節點 <song>
-        for (int i = 0; i < nl.getLength(); i++) {
-            // 新建一個 HashMap
-            HashMap<String, String> map = new HashMap<String, String>();
-            Element e = (Element) nl.item(i);
-            //每個子節點添加到HashMap關鍵= >值
-            map.put(TAG_PID, parser.getValue(e, TAG_PID));
-            map.put(TAG_PRODUCTS, parser.getValue(e, TAG_PRODUCTS));
-            map.put(TAG_NAME, parser.getValue(e, TAG_NAME));
-            // map.put(KEY_DURATION, parser.getValue(e, KEY_DURATION));
-            // map.put(KEY_THUMB_URL, parser.getValue(e, KEY_THUMB_URL));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_mysop, menu);
+        return true;
+    }
 
-            // HashList添加到數組列表
-            songsList.add(map);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
 
+        return super.onOptionsItemSelected(item);
+    }
+    /**
+     * Background Async Task to Load all product by making HTTP Request
+     * */
+    class LoadAllProducts extends AsyncTask<String, String, String> {
 
-        list = (ListView) findViewById(R.id.list);
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Mysop.this);
+            pDialog.setMessage("Loading products. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting All products from url
+         * */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            // getting JSON string from URL
+            JSONObject json = jParser.makeHttpRequest(url_all_products, "GET", params);
+
+            // Check your log cat for JSON reponse
+            Log.d("All Products: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // products found
+                    // Getting Array of Products
+                    products = json.getJSONArray(TAG_PRODUCTS);
+
+                    // looping through All Products
+                    for (int i = 0; i < products.length(); i++) {
+                        JSONObject c = products.getJSONObject(i);
+
+                        // Storing each json item in variable
+                        String id = c.getString(TAG_PID);
+                        String name = c.getString(TAG_NAME);
+
+                        // creating new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        map.put(TAG_PID, id);
+                        map.put(TAG_NAME, name);
+
+                        // adding HashList to ArrayList
+                        productsList.add(map);
+                    }
+                } else {
 
 
-        adapter = new LazyAdapter(this, songsList);
-        list.setAdapter(adapter);
 
-
-        //为單一列表行添加單擊事件
-
-        list.setOnItemClickListener(new OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                //這裏可以自由發揮，比如播放一首歌曲等等
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
 
-        });
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            for (int i = 0; i < products.length(); i++) {
+                items.add(productsList.get(i).get(TAG_PID)+"\n"+productsList.get(i).get(TAG_NAME));
+                listInput.setAdapter(adapter);
+            }
+        }
 
     }
 }
-        
-

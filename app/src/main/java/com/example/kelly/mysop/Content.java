703031a377a1a9b6c28ae64b773958e70c738f52
tina8857 <tina8857@yahoo.com.tw>
2015-08-05 12:11:19 +0800

    0805

--- app/src/main/java/com/example/kelly/mysop/Content.java
+++ app/src/main/java/com/example/kelly/mysop/Content.java
@@ -63,11 +63,13 @@ public class Content extends Activity {
     private static final String TAG_SOPNAME = "sopname";
     private static final String TAG_INTRO = "intro";
     private static final String TAG_USERNAME="username";
+    private static final String TAG_STARTRULE="start_rule";
     private static String NUMBER ="";
     private  static String DETAIL="";
     private static String SOPNAME="";
     private static String INTRO="";
     private static String USERNAME="";
+    private static String STARTRULE="";
 
     JSONArray products = null;
 
@@ -161,11 +163,12 @@ public class Content extends Activity {
             ArrayList params = new ArrayList();
             ArrayList params1 = new ArrayList();
 
+
             params1.add(new BasicNameValuePair("Sopnumber", Sopnumber) );
             params.add(new BasicNameValuePair("Sopnumber", Sopnumber) );
 
 
-            // json抓sop內容  json1抓評論 json2存評論
+            // json抓sop內容  json1抓評論
             JSONObject json = Content.this.jsonParser.makeHttpRequest(Content.url_create_product, "GET", params);
             JSONObject json1 = Content.this.jsonParser.makeHttpRequest(Content.url_create_product1, "GET", params1);
 
@@ -212,6 +215,7 @@ public class Content extends Activity {
                     NUMBER  = json.getString(TAG_NUMBER);
                     DETAIL = json.getString(TAG_DETAIL);
                     INTRO  = json.getString(TAG_INTRO);
+                    STARTRULE=json.getString(TAG_STARTRULE);
                 }else{
                    // System.out.println("HAHA NO"+json.getString(TAG_SOPNAME));
 
@@ -233,18 +237,45 @@ public class Content extends Activity {
             // dismiss the dialog after getting all products
             pDialog.dismiss();
 
-            //鍵盤手看這邊  別失敗啊！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
 
+            //動態listview 產生評論
             for (int i = 0; i < products.length(); i++) {
                 items.add(productsList.get(i).get(TAG_PID)+"\n"+productsList.get(i).get(TAG_NAME));
                 listInput.setAdapter(adapter);
                 inputText.setText("");
             }
 
+            //sop內容讀取
             title.setText(SOPNAME);
             subtitle.setText(INTRO);
             sopnumber.setText(NUMBER);
             Ctext.setText(DETAIL);
+
+            System.out.println("HELLO "+STARTRULE);
+            switch (STARTRULE){
+                case "1":
+                    cagetory.setText("人工啟動");
+                    break;
+                case "2":
+                    cagetory.setText("前一步驟完工");
+                    break;
+                case "3":
+                    cagetory.setText("Beacon");
+                    break;
+                case "4":
+                    cagetory.setText("QR code");
+                    break;
+                case "5":
+                    cagetory.setText("NFC");
+                    break;
+                case "6":
+                    cagetory.setText("定位");
+                    break;
+                case "7":
+                    cagetory.setText("時間到期");
+                    break;
+
+            }
 
         }
 
@@ -288,8 +319,6 @@ public class Content extends Activity {
                 var9.printStackTrace();
             }
             pDialog.dismiss();
-
-
 
             return null;
         }

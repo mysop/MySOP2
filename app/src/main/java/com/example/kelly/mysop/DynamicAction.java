2154518b0267e591e350809aa1374e0b8bb42846
Chris <cliff5345179@gmail.com>
2015-08-06 15:14:48 +0800

    8/6
    呼呼

--- app/src/main/java/com/example/kelly/mysop/DynamicAction.java
+++ app/src/main/java/com/example/kelly/mysop/DynamicAction.java
@@ -4,13 +4,27 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class DynamicAction extends Activity
@@ -25,11 +39,38 @@ public class DynamicAction extends Activity
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
     private CharSequence mTitle;
+    private ProgressDialog pDialog;
+    JSONParser jParser = new JSONParser();
+    ArrayList<HashMap<String, String>> productsList;
+    //private static String url_all_products = "http://localhost:8080/kelly/test_getall.jsp";
+    private static String url_all_products = "http://140.115.80.237/front/test_getall.jsp";
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_PRODUCTS = "products";
+    private static final String TAG_PID = "pid";
+    private static final String TAG_NAME = "name";
+
+    private ListView listInput;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> items;
+    JSONArray products = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dynamic_action);
+
+        listInput = (ListView)findViewById(R.id.list_dynamic);
+        items = new ArrayList<String>();
+        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,items);
+        listInput.setAdapter(adapter);
+
+        //Intent intent = this.getIntent();
+        //Bundle bundle = intent.getExtras();
+
+        // Hashmap for ListView
+        productsList = new ArrayList<HashMap<String, String>>();
+        // Loading products in Background Thread
+        new LoadAllProducts().execute();
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getFragmentManager().findFragmentById(R.id.navigation_drawer);
@@ -151,5 +192,84 @@ public class DynamicAction extends Activity
                     getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
+class LoadAllProducts extends AsyncTask<String, String, String> {
 
+    /**
+     * Before starting background thread Show Progress Dialog
+     * */
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        pDialog = new ProgressDialog(DynamicAction.this);
+        pDialog.setMessage("Loading products. Please wait...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+        pDialog.show();
+    }
+
+    /**
+     * getting All products from url
+     * */
+    protected String doInBackground(String... args) {
+        // Building Parameters
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        // getting JSON string from URL
+        JSONObject json = jParser.makeHttpRequest(url_all_products, "GET", params);
+
+        // Check your log cat for JSON reponse
+        Log.d("All Products: ", json.toString());
+
+        try {
+            // Checking for SUCCESS TAG
+            int success = json.getInt(TAG_SUCCESS);
+
+            if (success == 1) {
+                // products found
+                // Getting Array of Products
+                products = json.getJSONArray(TAG_PRODUCTS);
+
+                // looping through All Products
+                for (int i = 0; i < products.length(); i++) {
+                    JSONObject c = products.getJSONObject(i);
+
+                    // Storing each json item in variable
+                    String id = c.getString(TAG_PID);
+                    String name = c.getString(TAG_NAME);
+
+                    // creating new HashMap
+                    HashMap<String, String> map = new HashMap<String, String>();
+
+                    // adding each child node to HashMap key => value
+                    map.put(TAG_PID, id);
+                    map.put(TAG_NAME, name);
+
+                    // adding HashList to ArrayList
+                    productsList.add(map);
+                }
+            } else {
+
+
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * After completing background task Dismiss the progress dialog
+     * **/
+    protected void onPostExecute(String file_url) {
+        // dismiss the dialog after getting all products
+        pDialog.dismiss();
+        // updating UI from Background Thread
+        for (int i = 0; i < products.length(); i++) {
+            items.add(productsList.get(i).get(TAG_PID)+"\n"+productsList.get(i).get(TAG_NAME));
+            listInput.setAdapter(adapter);
+        }
+    }
+
+}
 }

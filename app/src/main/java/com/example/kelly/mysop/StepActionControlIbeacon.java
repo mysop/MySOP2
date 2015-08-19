576b0e77a17b062c4177b15c44d2982f124e5188
KellyHsu <kelly830619@gmail.com>
2015-08-19 18:14:40 +0800

    0819

--- app/src/main/java/com/example/kelly/mysop/StepActionControlIbeacon.java
+++ app/src/main/java/com/example/kelly/mysop/StepActionControlIbeacon.java
@@ -1,8 +1,10 @@
 package com.example.kelly.mysop;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
@@ -10,18 +12,43 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import org.altbeacon.beacon.*;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 
 public class StepActionControlIbeacon extends Activity implements BeaconConsumer {
 
     public static final String TAG = "BeaconsEverywhere";
     private BeaconManager beaconManager;
+    private ProgressDialog pDialog;
+    JSONParser jsonParser = new JSONParser();
+    //讀取 qrcode 圖片
+    private static String url_uuid = "http://140.115.80.237/front/mysop_ACibeacon.jsp";
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_UUID = "UUID";
+    String UUID = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_action_control_ibeacon);
+
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();//獲得當前的藍芽
+        if(adapter.isEnabled()!=true){//如果藍芽未開啟
+            //則打開藍芽(會問使用者)
+            //Intent enabler=new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            //startActivity(enabler);
+
+            //則打開藍芽(不問使用者)
+            adapter.enable();
+
+        }
 
         beaconManager = BeaconManager.getInstanceForApplication(this);
 
@@ -30,6 +57,11 @@ public class StepActionControlIbeacon extends Activity implements BeaconConsumer
 
         beaconManager.bind(this);
     }
+
+    public void refresh(){
+
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -39,7 +71,7 @@ public class StepActionControlIbeacon extends Activity implements BeaconConsumer
     @Override
     public void onBeaconServiceConnect() {
 
-        final Region region = new Region("myBeaons", Identifier.parse("E5C1BD75-E5DC-47F8-9E20-9C0711235B2B"), null, null);
+        final Region region = new Region("myBeaons", Identifier.parse(UUID), null, null);
 
         beaconManager.setMonitorNotifier(new MonitorNotifier() {
             @Override
@@ -110,4 +142,65 @@ public class StepActionControlIbeacon extends Activity implements BeaconConsumer
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Background Async Task to Load all product by making HTTP Request
+     * */
+    class Checkibeacon extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(StepActionControlIbeacon.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting All products from url
+         */
+        protected String doInBackground(String... args) {
+
+            String StepNumber = "2";
+
+            ArrayList params = new ArrayList();
+            params.add(new BasicNameValuePair("tepNumber", StepNumber));
+
+            // getting JSON string from URL
+            JSONObject json = jsonParser.makeHttpRequest(url_uuid, "GET", params);
+
+            // Check your log cat for JSON reponse
+            Log.d("All Products: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    UUID = json.getString(TAG_UUID);
+                } else {
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * *
+         */
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+        }
+    }
+
 }

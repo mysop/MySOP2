782617e6ceb317db209cfec16f83a5b11fdaedb4
KellyHsu <kelly830619@gmail.com>
2015-08-19 11:34:36 +0800

    0819
    ibeacon訊號抓到惹耶

--- app/src/main/java/com/example/kelly/mysop/StepActionControlIbeacon.java
+++ app/src/main/java/com/example/kelly/mysop/StepActionControlIbeacon.java
@@ -1,19 +1,88 @@
 package com.example.kelly.mysop;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.altbeacon.beacon.*;
+import java.util.Collection;
 
-public class StepActionControlIbeacon extends Activity {
+
+public class StepActionControlIbeacon extends Activity implements BeaconConsumer {
+
+    public static final String TAG = "BeaconsEverywhere";
+    private BeaconManager beaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_action_control_ibeacon);
+
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+
+        beaconManager.getBeaconParsers().add(new BeaconParser()
+                .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+
+        beaconManager.bind(this);
+
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
     }
 
+    @Override
+    public void onBeaconServiceConnect() {
+
+        final Region region = new Region("myBeaons", Identifier.parse("E5C1BD75-E5DC-47F8-9E20-9C0711235B2B"), null, null);
+
+        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                try {
+                    Log.d(TAG, "didEnterRegion");
+                    beaconManager.startRangingBeaconsInRegion(region);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void didExitRegion(Region region) {
+                try {
+                    Log.d(TAG, "didExitRegion");
+                    beaconManager.stopRangingBeaconsInRegion(region);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int i, Region region) {
+
+            }
+        });
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                for(Beacon oneBeacon : beacons) {
+                    Log.d(TAG, "distance: " + oneBeacon.getDistance() + " id:" + oneBeacon.getId1() + "/" + oneBeacon.getId2() + "/" + oneBeacon.getId3());
+                }
+            }
+        });
+
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

56970bef398e1b2c68d3dc2f390cccf06413fa18
KellyHsu <kelly830619@gmail.com>
2015-08-26 13:49:13 +0800

    0826

---
+++ app/src/main/java/com/example/kelly/mysop/StepNextControlUser.java
@@ -0,0 +1,212 @@
+package com.example.kelly.mysop;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+//p305
+public class StepNextControlUser extends Activity {
+
+
+    private ListView listView;
+    //private String[] list = {"鉛筆","原子筆","鋼筆","毛筆","彩色筆"};
+    private String[] list;
+    private ArrayAdapter<String> listAdapter;
+
+
+    private ProgressDialog pDialog;
+    JSONParser jParser = new JSONParser();
+    ArrayList<HashMap<String, String>> productsList;
+    JSONArray products = null;
+
+    private static String url_all_products = "http://140.115.80.237/front/mysop_steprecording2.jsp";
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_PRODUCTS = "products";
+    private static final String TAG_RECODE = "recode";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_next_control_user);
+
+        //list = new String[2];
+
+        listView = (ListView)findViewById(R.id.next_listView);
+/*        listAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(listAdapter);
+        listView.setOnItemClickListener(listener);*/
+
+        // Hashmap for ListView
+        productsList = new ArrayList<HashMap<String, String>>();
+        // Loading products in Background Thread
+        new LoadInput().execute();
+    }
+
+    private ListView.OnItemClickListener listener = new ListView.OnItemClickListener(){
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+            Toast.makeText(getApplicationContext(),"你選擇的是"+list[position], Toast.LENGTH_SHORT).show();
+        }
+
+    };
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_stepnextcontrol, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Background Async Task to Load all product by making HTTP Request
+     * */
+    class LoadInput extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(StepNextControlUser.this);
+            pDialog.setMessage("Loading. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting All products from url
+         * */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            // getting JSON string from URL
+            JSONObject json = jParser.makeHttpRequest(url_all_products, "GET", params);
+
+            // Check your log cat for JSON reponse
+            Log.d("All Products: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+
+                    products = json.getJSONArray(TAG_PRODUCTS);
+
+                    for (int i = 0; i < products.length(); i++) {
+                        JSONObject c = products.getJSONObject(i);
+
+
+                        // Storing each json item in variable
+                        int r = i+1;
+                        String id = c.getString(TAG_RECODE+r);
+
+
+                        // creating new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        map.put(TAG_RECODE+r, id);
+
+                        // adding HashList to ArrayList
+                        productsList.add(map);
+
+                    }
+                } else {
+
+
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+/*            runOnUiThread(new Runnable() {
+                public void run() {
+
+                }
+            }); */
+
+            list = new String[products.length()];
+
+            for(int i=0; i<products.length();i++) {
+
+                int r = i+1;
+                list[i] = productsList.get(i).get(TAG_RECODE+r);
+
+            }
+
+            listAdapter = new ArrayAdapter(StepNextControlUser.this,android.R.layout.simple_list_item_1,list);
+            listView.setAdapter(listAdapter);
+            listView.setOnItemClickListener(listener);
+
+
+/*            AlertDialog.Builder dialog = new AlertDialog.Builder(Steprecording.this);
+            dialog.setTitle("");
+            dialog.setMessage(productsList.get(1).get(TAG_RECODE + "3"));
+            dialog.show();*/
+
+
+        }
+
+    }
+
+
+
+
+}

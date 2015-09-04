ca85d78871df655dfe25bb9264f0ea2776225061
tina8857 <tina8857@yahoo.com.tw>
2015-09-04 16:23:03 +0800

    0904

--- app/src/main/java/com/example/kelly/mysop/Home.java
+++ app/src/main/java/com/example/kelly/mysop/Home.java
@@ -14,11 +14,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;

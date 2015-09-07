cacb5e2f5931afef43345d96269a4f3de92eb4f1
tina8857 <tina8857@yahoo.com.tw>
2015-09-07 21:53:53 +0800

    0907
    試試看grid layout for search

--- app/src/main/java/com/example/kelly/mysop/Search.java
+++ app/src/main/java/com/example/kelly/mysop/Search.java
@@ -5,15 +5,62 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import android.widget.AdapterView.OnItemClickListener;
 
 
 public class Search extends Activity {
+    private GridView gridView;
+    private int[] image = {
+            R.drawable.star1, R.drawable.star2, R.drawable.test,
+            R.drawable.test1, R.drawable.test2, R.drawable.test3,
+            R.drawable.errorpic, R.drawable.gps
+    };
+
+    private String[] imgText = {
+            "cat", "flower", "hippo", "monkey", "mushroom", "panda", "rabbit", "raccoon"
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        List<Map<String, Object>> items = new ArrayList<Map<String,Object>>();
+        for (int i = 0; i < image.length; i++) {
+            Map<String, Object> item = new HashMap<String, Object>();
+            item.put("image", image[i]);
+            item.put("text", imgText[i]);
+            items.add(item);
+        }
+        SimpleAdapter adapter = new SimpleAdapter(this,
+                items, R.layout.grid_item, new String[]{"image", "text"},
+                new int[]{R.id.image, R.id.text});
+
+        gridView = (GridView)findViewById(R.id.main_page_gridview);
+        gridView.setNumColumns(3);
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Your choice is "+imgText[position],
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
+
+
+
 
 
     @Override
@@ -37,4 +84,6 @@ public class Search extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }

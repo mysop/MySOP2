e5081d8b0516e2002a80145fc1cb5648fded326b
tina8857 <tina8857@yahoo.com.tw>
2015-09-08 15:54:12 +0800

    0908

--- app/src/main/java/com/example/kelly/mysop/Search.java
+++ app/src/main/java/com/example/kelly/mysop/Search.java
@@ -1,8 +1,11 @@
 package com.example.kelly.mysop;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,6 +20,11 @@ import java.util.List;
 import java.util.Map;
 import android.widget.AdapterView.OnItemClickListener;
 
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class Search extends Activity {
     private GridView gridView;
@@ -26,37 +34,50 @@ public class Search extends Activity {
             R.drawable.errorpic, R.drawable.gps
     };
 
-    private String[] imgText = {
-            "cat", "flower", "hippo", "monkey", "mushroom", "panda", "rabbit", "raccoon"
-    };
+    private String[] imgText ;
+    private String[] imgText2;
+    private ProgressDialog pDialog;
+    JSONParser jsonParser = new JSONParser();
+    ArrayList<HashMap<String, String>> productsList;
+    private static String url_all_products = "http://140.115.80.237/front/mysop_home.jsp";
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_PRODUCTS = "products";
+    private static final String TAG_USERNAME = "username";
+    private static final String TAG_SOPNAME = "sopname";
+    private static final String TAG_PICTURE = "picture";
+    JSONArray products = null;
+    List<Map<String, Object>> items = new ArrayList<Map<String,Object>>();
+    SimpleAdapter adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        List<Map<String, Object>> items = new ArrayList<Map<String,Object>>();
+
         for (int i = 0; i < image.length; i++) {
             Map<String, Object> item = new HashMap<String, Object>();
             item.put("image", image[i]);
             item.put("text", imgText[i]);
+            item.put("text2",imgText2[i]);
             items.add(item);
         }
-        SimpleAdapter adapter = new SimpleAdapter(this,
-                items, R.layout.grid_item, new String[]{"image", "text"},
-                new int[]{R.id.image, R.id.text});
+        adapter = new SimpleAdapter(this,
+                items, R.layout.grid_item, new String[]{"image", "text","text2"},
+                new int[]{R.id.image, R.id.text,R.id.text2});
 
         gridView = (GridView)findViewById(R.id.main_page_gridview);
-        gridView.setNumColumns(3);
+        gridView.setNumColumns(2);
         gridView.setAdapter(adapter);
-        gridView.setOnItemClickListener(new OnItemClickListener(){
+        gridView.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "Your choice is "+imgText[position],
+                Toast.makeText(getApplicationContext(), "Your choice is " + imgText[position],
                         Toast.LENGTH_SHORT).show();
             }
 
         });
+        new LoadSearch().execute();
     }
 
 
@@ -85,5 +106,107 @@ public class Search extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    class LoadSearch extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Search.this);
+            pDialog.setMessage("Loading products. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting All products from url
+         * */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            // params.add(new BasicNameValuePair("Account", TAG_ACCOUNT) );
+            // getting JSON string from URL
+            JSONObject json = Search.this.jsonParser.makeHttpRequest(Search.url_all_products,"GET", params);
+
+
+            // Check your log cat for JSON reponse
+            Log.d("All Products: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // products found
+                    // Getting Array of Products
+                    products = json.getJSONArray(TAG_PRODUCTS);
+
+                    // looping through All Products
+                    for (int i = 0; i < products.length(); i++) {
+                        JSONObject c = products.getJSONObject(i);
+
+                        // Storing each json item in variable
+                        String sopname = c.getString(TAG_SOPNAME);
+                        String username = c.getString(TAG_USERNAME);
+                        String picture = c.getString(TAG_PICTURE);
+
+                        // creating new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        map.put(TAG_SOPNAME, sopname);
+                        map.put(TAG_USERNAME, username);
+                        map.put(TAG_PICTURE, picture);
+
+
+                        // adding HashList to ArrayList
+                        productsList.add(map);
+                    }
+                } else {
+
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+
+            imgText = new String[products.length()];
+            imgText2 = new String[products.length()];
+
+
+//                for (int i = 0; i <products.length(); i=i+2) {
+//                    imgText[i] = productsList.get(i).get(TAG_SOPNAME);
+//                    imgText2[i] = productsList.get(i).get(TAG_USERNAME);
+//                }
+//
+//            List<Map<String, Object>> items = new ArrayList<Map<String,Object>>();
+//            for (int i = 0; i < image.length; i++) {
+//                Map<String, Object> item = new HashMap<String, Object>();
+//                item.put("image", image[i]);
+//                item.put("text", imgText[i]);
+//                item.put("text2",imgText2[i]);
+//                items.add(item);
+//            }
+//
+//            gridView = (GridView)findViewById(R.id.main_page_gridview);
+//            gridView.setNumColumns(2);
+//            gridView.setAdapter(adapter);
+
+        }
+
+    }
 
 }

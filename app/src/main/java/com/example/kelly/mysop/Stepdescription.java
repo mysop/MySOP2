892dcf0ffe3b46469d234f68f15a80f46e5ea617
KellyHsu <kelly830619@gmail.com>
2015-09-16 11:23:00 +0800

    0916
    修

--- app/src/main/java/com/example/kelly/mysop/Stepdescription.java
+++ app/src/main/java/com/example/kelly/mysop/Stepdescription.java
@@ -1,14 +1,27 @@
 package com.example.kelly.mysop;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebView;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 //p302
@@ -19,6 +32,16 @@ public class Stepdescription extends Activity {
     String TAG_CASE_NUMBER = "";
     String TAG_STEP_NUMBER = "";
     int TAG_STEP_ORDER = 0;
+
+
+    private ProgressDialog pDialog;
+    JSONParser jParser = new JSONParser();
+    ArrayList<HashMap<String, String>> productsList;
+    JSONArray products = null;
+
+    private static String url_next = "http://140.115.80.237/front/mysop_stepdescription.jsp";
+    private static final String TAG_SUCCESS = "success";
+    private int TAG_Next=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +54,62 @@ public class Stepdescription extends Activity {
         TAG_STEP_NUMBER = bundle.getString("TAG_STEP_NUMBER");
         TAG_STEP_ORDER = bundle.getInt("TAG_STEP_ORDER");
 
+        //TAG_CASE_NUMBER = "J1234";
+        //TAG_STEP_NUMBER = "10";
+        //TAG_STEP_ORDER = 1;
+        new CheckNextActivity().execute();
+
         WebView wv = (WebView)findViewById(R.id.webView);
         wv.loadUrl("https://www.google.com.tw");
         detector = new GestureDetector(new MySimpleOnGestureListener());
         WebView ww = (WebView)findViewById(R.id.webView);
         ww.setOnTouchListener(new MyOnTouchListener());
+    }
+
+    class CheckNextActivity extends AsyncTask<String, String, Integer> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //pDialog = new ProgressDialog(StepActionControl.this);
+            //pDialog.setMessage("Loading..... Please wait...");
+            //pDialog.setIndeterminate(false);
+            //pDialog.setCancelable(false);
+            //pDialog.show();
+        }
+        protected Integer doInBackground(String... args) {
+
+            int NextActivity=0;
+            String Stepnumber = TAG_STEP_NUMBER;
+
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("Stepnumber", Stepnumber));
+
+            JSONObject json = jParser.makeHttpRequest(url_next, "GET", params);
+
+            Log.d("All Products: ", json.toString());
+
+            try {
+
+                int success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    NextActivity=1;
+                    TAG_Next = NextActivity;
+                } else if(success == 6) {
+                    NextActivity=2;
+                    TAG_Next = NextActivity;
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return NextActivity;
+        }
+        protected void onPostExecute(Integer NextActivity) {
+            if(NextActivity==0){
+               Log.d("下一步","讀取失敗");
+            }
+        }
+
     }
 
 
@@ -73,7 +147,11 @@ public class Stepdescription extends Activity {
             // TODO Auto-generated method stub
             if ((e1.getX() - e2.getX()) > 50) {//说明是左滑
                 Intent intent = new Intent();
-                intent.setClass(Stepdescription.this, Steprecording.class);
+                if(TAG_Next==1){
+                    intent.setClass(Stepdescription.this, Steprecording.class);
+                }else{
+                    intent.setClass(Stepdescription.this, StepCutControl.class);
+                }
                 Bundle bundle = new Bundle();
                 bundle.putString("TAG_CASE_NUMBER",TAG_CASE_NUMBER);
                 bundle.putString("TAG_STEP_NUMBER", TAG_STEP_NUMBER);
